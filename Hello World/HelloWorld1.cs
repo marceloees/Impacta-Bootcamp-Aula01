8268fe82bd81d61ca889880b1728cd8a4b52e1e9
marceloees <marceloees17@gmail.com>
2021-09-01 16:52:30 -0300

    Aula02
    Exercícios estrutura de condição

--- Hello World/HelloWorld1.cs
+++ Hello World/HelloWorld1.cs
@@ -7,7 +7,29 @@ namespace Hello_World
     class HelloWorld1
     {
         static void Main(string[] args) {
-            Console.WriteLine("Marcelo Eduardo");
+            int idade = 18;
+            bool isAdulto;
+            decimal valor = 10.55m;
+            double valor2 = 10.55;
+            char sigla = 'C';
+            string estado = "São Paulo";
+            var teste = "teste";
+
+            if (idade >= 18 && estado == "São Paulo")
+            {
+                valor2 = +3;
+                Console.WriteLine(valor2);
+            }
+            else if (idade <= 36 && estado == "Sergipe")
+            {
+                valor2 =- 3;
+                Console.WriteLine(valor2);
+            }
+            
+            }
+
+
+            
+
         }
     }
-}
