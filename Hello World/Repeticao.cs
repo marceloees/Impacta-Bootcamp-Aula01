242091a1644deb1a2568a79671a6047046cc313f
marceloees <marceloees17@gmail.com>
2021-09-02 18:09:45 -0300

    Aula 02

--- Hello World/Repeticao.cs
+++ Hello World/Repeticao.cs
@@ -6,30 +6,14 @@ namespace Hello_World
 {
     class Repeticao
     {
-        static void Main(string[] argumentos) {
-            for (var loops = 0; loops < argumentos.Length ; loops++) {
-                var argumento = argumentos[loops];
-                Console.WriteLine($"Estamos com {loops}");
+        static void Main(string[] argumentos)
+        {
+            foreach (var h in argumentos) {
+                Console.WriteLine($"Cante comigo: {h}");
             }
 
-            foreach (var argumento in argumentos) {
-                Console.WriteLine($"Argumento lido : {argumento}");
-            }
 
-            var loops2 = 0;
-            while (loops2 < argumentos.Length) {
-                var argumento = argumentos[loops2];
-                Console.WriteLine($"Veeem fi {argumento}");
-                loops2++;
-            }
 
-            var loops3 = 0;
-            do
-            {
-                var argumento = argumentos[loops3];
-                Console.WriteLine($"Veeem fi {argumento}");
-                loops3++;
-            } while (loops3 < argumentos.Length);
         }
     }
 }
